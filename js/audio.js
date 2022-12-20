53a6349f2f5f73c17096c0ebc762ee6010a5afe3
SOM-0x3B1 <somadagaszkar@gmail.com>
2022-12-20 22:56:23 +0100

    Reconfigure loading animations

---
+++ js/audio.js
@@ -0,0 +1,10 @@
+const aconnect = new Audio('media/connect.mp3');
+const abg = new Audio('media/bg.mp3');
+const astartup = new Audio('media/startup.mp3');
+const aupdate = new Audio('media/update.mp3');
+const alogin = new Audio('media/login.mp3');
+const aend = new Audio('media/end.mp3');
+const ashow = new Audio('media/show.mp3');
+const aclose = new Audio('media/close.mp3');
+
+abg.loop = true;
---
+++ js/progress.js
@@ -0,0 +1,132 @@
+const countDownDate = new Date("2022-011-02T21:00:00.000+02:00").getTime();
+const originDate = new Date("2022-02-04T21:00:00.000+02:00").getTime();
+
+// Calculate milliseconds in a year
+const pSecond = 1000;
+const pMinute = pSecond * 60;
+const pHour = pMinute * 60;
+const pDay = pHour * 24;
+
+const cTitle1Text = "REBOOTING UNIVERSE";
+const cTitle2text = "PLEASE STAND BY";
+
+async function connect() {
+    aconnect.play();
+    document.getElementById('connect').remove();
+    //try { document.getElementById('ERROR').remove(); } catch{}
+
+    astartup.play();
+
+    let cursor = document.getElementById('cursor');
+    cursor.style.display = 'inline-block';
+
+    let cTitle1 = document.getElementById('cTitle1Text');
+    for (let i = 0; i < cTitle1Text.length; i++) {
+        cTitle1.innerHTML += cTitle1Text[i];
+        await sleep(50);
+    }
+    await sleep(200);
+
+    document.getElementById('cTitle2').appendChild(cursor);
+    let cTitle2 = document.getElementById('cTitle2Text');    
+    for (let i = 0; i < cTitle2text.length; i++) {
+        cTitle2.innerHTML += cTitle2text[i];
+        await sleep(50);
+    }
+
+    cursor.style.animation = 'blink 0.9s infinite';
+
+    await sleep(1200);
+    let progressbar = document.getElementById('fullProgressBar');
+    progressbar.style.border = 'solid var(--foreground01) 2pt';
+    progressbar.style.width = '100%';
+
+    await sleep(300);
+    let percentage = document.getElementById('percentage');
+    percentage.style.display = 'inline-block';   
+    aupdate.play();
+
+    await sleep(200);
+    let ETRLabel = document.getElementById('ETRLabel');
+    ETRLabel.style.opacity = 1;
+    await sleep(30);
+    ETRLabel.style.backgroundColor = 'var(--background01)';
+    ETRLabel.style.boxShadow = 'none';
+
+    await sleep(100);
+    let ETRValue = document.getElementById('ETRValue');
+    ETRValue.style.opacity = 1;
+    await sleep(100);
+    ETRValue.style.backgroundColor = 'var(--background01)';
+    ETRValue.style.boxShadow = 'none';
+
+    startCountDown();
+}
+
+async function startCountDown() {
+    abg.play();
+
+    let lastPercentage = "0%";
+
+    let x = setInterval(async () => {
+        let now = new Date().getTime();
+
+        // Find the difference between now and the count down date
+        let remaining = countDownDate - now;
+
+        if (remaining < 0) {
+            clearInterval(x);
+
+            aend.play();
+            document.getElementById('h2').innerHTML = 'REBOOT COMPLETE<br>THANK YOU FOR WATCHING';
+            document.getElementById('cursor').innerText = '♥';
+            document.getElementById("cursor").style.fontSize = '20pt';
+
+            document.getElementById("percentage").innerText = '100%';
+            document.getElementById("fill").style.width = '100%';
+            document.getElementById("p_1").innerText = "THE WEBSITE WILL BE REPURPOSED,";
+            document.getElementById("time").innerText = "so don't forget about this place".toUpperCase();
+
+            await sleep(2000);
+            aupdate.play();
+            document.getElementById("info").style.display = 'inline-block';
+        }
+        else {
+
+            // Time calculations for days, hours, minutes and seconds
+            let days = Math.floor(remaining / pDay);
+            let hours = Math.floor((remaining % (pDay)) / (pHour));
+            let minutes = Math.floor((remaining % (pHour)) / (pMinute));
+            let seconds = Math.floor((remaining % (pMinute)) / pSecond);
+
+            // Display the result in the element with id="demo"
+            document.getElementById("time").innerText = days + " DAYS " + hours + " HOURS "
+                + minutes + " MINUTES " + seconds + " SECONDS ";
+
+            let percentage = (Math.floor((100 - (countDownDate - now) / (countDownDate - originDate) * 100) * 100) / 100) + '%'
+
+
+            if (percentage != lastPercentage) {
+                aupdate.play();
+                lastPercentage = percentage;
+                let perc1 = document.getElementById("percentage");
+                perc1.innerText = percentage;
+                document.getElementById("fill").style.width = percentage;
+            }
+        }
+    }, 1000);
+}
+
+
+function showInfo() {
+    ashow.play();
+    document.getElementById("infopanel").style.display = 'inline-block';
+}
+function closeInfo() {
+    aclose.play();
+    document.getElementById("infopanel").style.display = 'none';
+}
+
+async function sleep(ms) {
+    await new Promise(r => setTimeout(r, ms));
+}
